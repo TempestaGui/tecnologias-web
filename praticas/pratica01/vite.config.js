c4ab080200debc33abecc9e175efca8183bf5b26
DevGuilhermeTempesta <guilhermeftempesta@gmail.com>
2025-03-06 21:57:55 -0300

    agora esta funcionando

--- praticas/pratica01/vite.config.js
+++ praticas/pratica01/vite.config.js
@@ -1,13 +1,13 @@
-export default{
-    apptype: "mpa",
+export default {
+    appType: "mpa",
     build: {
-        rollupOptions: {
-            input: {
-                index: "index.html",
-                login: "./src/login.html",
-                about: "./src/home.html",
-                perfil: "./src/perfil.html",
-            },
-        },
+    rollupOptions: {
+    input: {
+    index: "index.html",
+    login: "./src/login.html",
+    about: "./src/home.html",
+    perfil: "./src/perfil.html",
     },
-};
+    },
+    },
+    };
